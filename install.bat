4fae28d9732508fd7d5df54238b5ab403819cefd
José Ignacio Requeno <nrequeno@unizar.es>
2022-02-11 00:17:15 +0100

    Renaming install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
-#pip2 uninstall ParetoLib
+pip install -r requirements.txt --user
+::pip uninstall ParetoLib
 python setup.py clean --all
 python setup.py build
 python setup.py install --force --user
