e08310112c79323c3e6d1c2174e54103b776b57f
José Ignacio Requeno <nrequeno@unizar.es>
2022-02-16 16:52:40 +0100

    - New STLe binaries/dlls including derivative operator - New Test/triangular using derivatives

--- install.bat
+++ install.bat
@@ -1,5 +1,5 @@
 pip install -r requirements.txt --user
-pip uninstall ParetoLib
+::pip uninstall ParetoLib
 python setup2.py clean --all
 python setup2.py build
 python setup2.py install --force --user
