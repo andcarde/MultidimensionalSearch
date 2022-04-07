f7cf27a277d20348c281fda7413e3f31bf8c32b5
José Ignacio Requeno <nrequeno@unizar.es>
2022-04-07 10:32:09 +0200

    - Reformatting code. - New run_stle

--- ParetoLib/GUI/GUI.py
+++ ParetoLib/GUI/GUI.py
@@ -2,8 +2,10 @@ from qtpy import QtWidgets
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import tempfile
 
-from Window import Ui_MainWindow
+from ParetoLib.GUI.Window import Ui_MainWindow
+from ParetoLib.Oracle.OracleSTLe import OracleSTLeLib
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -11,15 +13,16 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
         self.setupUi(self)
         # Conectamos los eventos con sus acciones
-        self.f_senial_entrada_button.clicked.connect(self.plot_csv)
+        self.f_senial_entrada_button.clicked.connect(self.plot_signal)
 
-    def plot_csv(self):
-        # Leer ruta csvfile de self.label_3
-        csvfile = "../../Tests/Oracle/OracleSTLe/2D/stabilization/stabilization.csv"
+    def plot_signal(self):
+        # Reading csvfile from self.label_3
+        # csvfile = "../../Tests/Oracle/OracleSTLe/2D/stabilization/stabilization.csv"
+        csv_signal_file = self.f_senial_entrada_textbox.toPlainText()
 
         # Read CSV file
         names = ["Time", "Signal"]
-        df_signal = pd.read_csv(csvfile, names=names)
+        df_signal = pd.read_csv(csv_signal_file, names=names)
 
         # Plot the responses for different events and regions
         sns.set_theme(style="darkgrid")
@@ -27,6 +30,22 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                            y="Signal",
                            data=df_signal)
         plt.show()
+
+    def run_stle(self):
+        # Running STLEval without parameters
+        stl_prop_file = self.f_especificacion_textbox.toPlainText()
+        csv_signal_file = self.f_senial_entrada_textbox.toPlainText()
+        # No parameters (i.e., using empty temporary file)
+        stl_param = tempfile.NamedTemporaryFile(mode='r')
+        stl_param_file = stl_param.name
+
+        # Initialize the OracleSTLeLib
+        oracle = OracleSTLeLib(stl_prop_file, csv_signal_file, stl_param_file)
+
+        # Evaluate the STLe expression
+        stl_formula = oracle._load_stl_formula(stl_prop_file)
+        satisfied = oracle.eval_stl_formula(stl_formula)
+        return satisfied
 
 
 if __name__ == "__main__":
