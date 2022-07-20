d8856188b42802d17810b6af3291f5483aac65b8
José Ignacio Requeno <nrequeno@unizar.es>
2022-07-20 13:04:08 +0200

    Cleaning examples

--- doc/examples/OracleSTLe/example2d_derivative.py
+++ doc/examples/OracleSTLe/example2d_derivative.py
@@ -2,12 +2,23 @@ from ParetoLib.Oracle.OracleSTLe import OracleSTLeLib
 from ParetoLib.Search.Search import Search2D, EPS, DELTA, STEPS
 
 # File containing the definition of the Oracle
-nfile = '../../../Tests/Oracle/OracleSTLe/2D/triangular/derivative/triangular.txt'
+# nfile = '../../../Tests/Oracle/OracleSTLe/2D/triangular/derivative/triangular_int.txt'
+# nfile = '../../../Tests/Oracle/OracleSTLe/2D/triangular/derivative/triangular_float.txt'
+nfile = '../../../Tests/Oracle/OracleSTLe/2D/stabilization/derivative/stabilization.txt'
 human_readable = True
 
 # Definition of the n-dimensional space
-min_x, min_y = (1.0, 0.0)
-max_x, max_y = (999.0, 2.0)
+# For triangular_int:
+# min_x, min_y = (1.0, 0.0)
+# max_x, max_y = (999.0, 2.0)
+
+# For triangular_float:
+# min_x, min_y = (0.0, 0.0)
+# max_x, max_y = (10.0, 0.002)
+
+# For stabilization_der:
+min_x, min_y = (80.0, 0.0)
+max_x, max_y = (120.0, 0.005)
 
 oracle = OracleSTLeLib()
 oracle.from_file(nfile, human_readable)
