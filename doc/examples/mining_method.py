10cb6c680bd64bcf27eb8a46363229b3ba96b741
José Ignacio Requeno <nrequeno@unizar.es>
2022-09-17 21:46:45 +0200

    Working

--- doc/examples/mining_method.py
+++ doc/examples/mining_method.py
@@ -24,15 +24,20 @@ from ParetoLib.Search.ResultSet import ResultSet
 def cell_partition(pspace: Rectangle, num_cells: int) -> List:
     d = pspace.dim()
     # num_cells = k^d
-    k = log(num_cells, d)
-    k = floor(k) + 1
+    k = log(num_cells, 10)/d
+    print("k: {0}".format(k))
+    k = floor(pow(10, k)) + 1
+    print("k: {0}".format(k))
     step = np.subtract(pspace.max_corner, pspace.min_corner)
     step = np.divide(step, k)
-    print("k: {0}, step: {1}".format(k, step))
+    print("step: {0}".format(step))
     indices_min_corners = product(range(k), repeat=d)
     list_min_corners = (np.add(pspace.min_corner, np.multiply(index, step)) for index in indices_min_corners)
     res = [Rectangle(min_corner, np.add(min_corner, step)) for min_corner in list_min_corners]
+    # print("res: {0} len: {1}".format(res, len(res)))
+    print("len(res): {0}".format(len(res)))
     return res
+
 
 def mining_method(pspace: Rectangle, num_cells: int) -> ResultSet:
     cells = cell_partition(pspace, num_cells)
@@ -40,13 +45,18 @@ def mining_method(pspace: Rectangle, num_cells: int) -> ResultSet:
     undef = []
     green = []
     red = []
-    for i in range(num_cells // 2):
+    half = num_cells // 2
+    for i in range(half):
         green.append(cells[i])
-        red.append(cells[i + 1])
+
+    for i in range(half, num_cells):
+        red.append(cells[i])
+
     return ResultSet(undef, red, green, pspace)
 
 
 if __name__ == "__main__":
     n = int(sys.argv[1])
     pspace = Rectangle((0.0,) * 3, (1.0,) * 3)
-    mining_method(pspace, n)
+    r = mining_method(pspace, n)
+    r.plot_3D()
