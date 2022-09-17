12d0337a1057c9e54226cb35b84e335970670f19
José Ignacio Requeno <nrequeno@unizar.es>
2022-09-17 18:01:04 +0200

    First draft

---
+++ doc/examples/mining_method.py
@@ -0,0 +1,52 @@
+import sys
+
+from typing import List
+from math import log, floor
+from itertools import product
+import numpy as np
+# from ParetoLib.Geometry.Point import add, subtract, mult, div
+from ParetoLib.Geometry.Rectangle import Rectangle
+from ParetoLib.Search.ResultSet import ResultSet
+
+
+# def cell_partition(pspace: Rectangle, num_cells: int) -> List:
+#     d = pspace.dim()
+#     # num_cells = k^d
+#     k = log(num_cells, d)
+#     k = floor(k)
+#     step = subtract(pspace.max_corner, pspace.min_corner)
+#     step = div(step, k)
+#     indices_min_corners = product(range(k), repeat=d)
+#     list_min_corners = (add(pspace.min_corner, mult(index, step)) for index in indices_min_corners)
+#     res = [Rectangle(min_corner, add(min_corner, step)) for min_corner in list_min_corners]
+#     return res
+
+def cell_partition(pspace: Rectangle, num_cells: int) -> List:
+    d = pspace.dim()
+    # num_cells = k^d
+    k = log(num_cells, d)
+    k = floor(k) + 1
+    step = np.subtract(pspace.max_corner, pspace.min_corner)
+    step = np.divide(step, k)
+    print("k: {0}, step: {1}".format(k, step))
+    indices_min_corners = product(range(k), repeat=d)
+    list_min_corners = (np.add(pspace.min_corner, np.multiply(index, step)) for index in indices_min_corners)
+    res = [Rectangle(min_corner, np.add(min_corner, step)) for min_corner in list_min_corners]
+    return res
+
+def mining_method(pspace: Rectangle, num_cells: int) -> ResultSet:
+    cells = cell_partition(pspace, num_cells)
+    num_cells = len(cells)
+    undef = []
+    green = []
+    red = []
+    for i in range(num_cells // 2):
+        green.append(cells[i])
+        red.append(cells[i + 1])
+    return ResultSet(undef, red, green, pspace)
+
+
+if __name__ == "__main__":
+    n = int(sys.argv[1])
+    pspace = Rectangle((0.0,) * 3, (1.0,) * 3)
+    mining_method(pspace, n)
