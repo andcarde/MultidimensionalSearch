ca1e4f3e10dcc42459adf5e27743997db921573b
nachorequeno <nrequeno@unizar.es>
2022-09-23 10:17:32 +0200

    Cleaning code

--- doc/examples/mining_method_skeleton.py
+++ doc/examples/mining_method_skeleton.py
@@ -1,24 +1,26 @@
-import matplotlib.pyplot as plt
-import operator
+# Look at numpy and ParetoLib.Geometry.Point libraries
+# import operator
+import numpy as np
 from ParetoLib.Geometry.Rectangle import Rectangle
 from ParetoLib.Search.ResultSet import ResultSet
 
+# def opera_tupla(op, tup1, tup2):
+#     assert (len(tup1) == len(tup2))
+#     return tuple(map(op, tup1, tup2))
 
-def opera_tupla(op,tup1,tup2):
-    assert(len(tup1) == len(tup2))
-    return tuple(map(op,tup1,tup2))
 
-def escalar_tupla(op,esc,tup):
-    return tuple([op(esc,x) for x in tup])
-
+# def escalar_tupla(op, esc, tup):
+#     return tuple([op(esc, x) for x in tup])
 
 
 # mining_method(pspace: Rectangle, n: int) -> ResultSet. Divide pspace into n Rectangles of the same size and classify them
 def mining_method(pspace, n):
-    verts = pspace._vertices()
-    half = len(verts)//2
-    ver_dist = opera_tupla(operator.sub,verts[half],verts[0])
-    rect_list = [Rectangle(opera_tupla(operator.add,verts[0],escalar_tupla(operator.mul,i/n,ver_dist)),opera_tupla(operator.add,verts[half-1],escalar_tupla(operator.mul,(i+1)/n,ver_dist))) for i in range(n)]
+    verts = pspace.vertices() # Methods that start by _ usually are private methods
+    half = len(verts) // 2
+    # max_corner = verts[half], min_corner = verts[0]?
+    ver_dist = np.subtract(verts[half], verts[0]) # Equivalent to pspace.diag_vector() ?
+    rect_list = [Rectangle(np.add(verts[0], np.multiply(ver_dist, i / n)),
+                           np.add(verts[half - 1], np.multiply(ver_dist, (i + 1) / n))) for i in range(n)]
 
     ### Approach 1: Vertex matrix. Discarded for the moment ###
 
@@ -27,24 +29,25 @@ def mining_method(pspace, n):
     #    ver_ini = verts[i]
     #    ver_dist = opera_tupla(operator.sub,verts[i+half],verts[i])
     #    mat_verts.append([opera_tupla(operator.add,ver_ini,escalar_tupla(operator.mul,j/n,ver_dist)) for j in range(n+1)])
-    
+
     # rect_list = [Rectangle(mat_verts[0][i],mat_verts[len(mat_verts)-1][i+1]) for i in range(len(mat_verts[0])-1)]
 
-    return ResultSet(yup=rect_list[:len(rect_list)//2],ylow=rect_list[len(rect_list)//2:],xspace=pspace)
+    # n//2 should be equivalent
+    return ResultSet(yup=rect_list[:n // 2], ylow=rect_list[n // 2:], xspace=pspace)
 
-def plot_prueba(min_cor,max_cor,n):
-    space = Rectangle(min_cor,max_cor)
-    rs = mining_method(space,n)
-    if len(min_cor) == 2:
+
+def plot_prueba(min_cor, max_cor, n):
+    space = Rectangle(min_cor, max_cor)
+    rs = mining_method(space, n)
+    # It's better to check the dim() rather than the len.
+    if space.dim() == 2:
         rs.plot_2D()
-    else:
+    elif space.dim() == 3:
         rs.plot_3D()
 
 
-
-min_cor = (1,2,3)
-max_cor = (5,3,7)
-n = 8
-plot_prueba(min_cor,max_cor,n)
-
-
+if __name__ == "__main__":
+    min_cor = (1, 2, 3)
+    max_cor = (5, 3, 7)
+    n = 10
+    plot_prueba(min_cor, max_cor, n)
