35f22be7f6fa1d3d8202184c3e346d4d5f866f4c
nachorequeno <nrequeno@unizar.es>
2022-09-23 16:28:20 +0200

    Task next week completed

--- doc/examples/mining_method.py
+++ doc/examples/mining_method.py
@@ -4,65 +4,75 @@ from typing import List
 from math import log, ceil
 from itertools import product
 import numpy as np
-# from ParetoLib.Geometry.Point import add, subtract, mult, div
 from ParetoLib.Geometry.Rectangle import Rectangle
 from ParetoLib.Search.ResultSet import ResultSet
 from ParetoLib.Oracle.OracleSTLe import OracleSTLeLib
 
 
-# def cell_partition(pspace: Rectangle, num_cells: int) -> List:
-#     d = pspace.dim()
-#     # num_cells = k^d
-#     k = log(num_cells, d)
-#     k = floor(k)
-#     step = subtract(pspace.max_corner, pspace.min_corner)
-#     step = div(step, k)
-#     indices_min_corners = product(range(k), repeat=d)
-#     list_min_corners = (add(pspace.min_corner, mult(index, step)) for index in indices_min_corners)
-#     res = [Rectangle(min_corner, add(min_corner, step)) for min_corner in list_min_corners]
-#     return res
+def confidence(p0: float, alpha: float) -> int:
+    """As a concrete example, let us set p0 = 0.01 and alpha = 0.05. In this setting, we need to examine
+    N >= log(1−p0) alpha = log0.99(0.05) = 298 parameter valuations to conclude with confidence 95% that the proportion
+    of satisfied parameter valuation in the cell is smaller than 1%."""
+    n = log(alpha, 1-p0)
+    return ceil(n)
+
 
 def cell_partition(pspace: Rectangle, num_cells: int) -> List:
     d = pspace.dim()
+
     # num_cells = k^d
     k = log(num_cells, 10)/d
-    print("k: {0}".format(k))
     k = ceil(pow(10, k))
-    print("k: {0}".format(k))
+
     step = np.subtract(pspace.max_corner, pspace.min_corner)
     step = np.divide(step, k)
-    print("step: {0}".format(step))
+
     indices_min_corners = product(range(k), repeat=d)
     list_min_corners = (np.add(pspace.min_corner, np.multiply(index, step)) for index in indices_min_corners)
     res = [Rectangle(min_corner, np.add(min_corner, step)) for min_corner in list_min_corners]
-    # print("res: {0} len: {1}".format(res, len(res)))
-    print("len(res): {0}".format(len(res)))
     return res
 
 
 # Inicializacion del Oracle
 # Num_muestras
-def mining_method(pspace: Rectangle, num_cells: int, num_muestras: int, oracle: OracleSTLeLib) -> ResultSet:
+def mining_method(pspace: Rectangle, num_cells: int, num_samples: int, oracle: OracleSTLeLib) -> ResultSet:
     cells = cell_partition(pspace, num_cells)
-    num_cells = len(cells)
     undef = []
     green = []
     red = []
 
+    d = pspace.dim()
     f = oracle.membership()
     for cell in cells:
-        # Tomar N muestras uniformes (np.uniform), con las coordenadas min,max de cell
-        num_muestras = list()
-        for muestra in num_muestras:
-            # Llamar al oraculo con una muestra
-            f(muestra)
-            # Si las N muestras son ciertas, se anade cell a verdes, sino a rojas
+        # Take num_samples uniformly between cell.min_corner and cell.max_corner
+        samples = np.random.uniform(cell.min_corner, cell.max_corner, (num_samples, d))
+        # Call the oracle with the current sample
+        eval_samples = (f(sample) for sample in samples)
+        # If all the samples eval to True, then the cell goes to green. Otherwise, cell goes to red.
+        if all(eval_samples):
+            green.append(cell)
+        else:
+            red.append(cell)
 
     return ResultSet(undef, red, green, pspace)
 
 
 if __name__ == "__main__":
-    n = int(sys.argv[1])
-    pspace = Rectangle((0.0,) * 3, (1.0,) * 3)
-    r = mining_method(pspace, n)
-    r.plot_3D()
+    # python3 this_file.py num_cells p0 alpha
+    # python3 mining_method.py 1000 0.01 0.05
+
+    num_cells = int(sys.argv[1])
+    p0 = float(sys.argv[2])
+    alpha = float(sys.argv[3])
+
+    nfile = '../../Tests/Oracle/OracleSTLe/2D/stabilization/derivative/stabilization.txt'
+    oracle = OracleSTLeLib()
+    oracle.from_file(nfile, human_readable=True)
+
+    num_samples = confidence(p0, alpha)
+    # pspace = Rectangle((0.0,) * 3, (1.0,) * 3)
+    min_x, min_y = (80.0, 0.0)
+    max_x, max_y = (120.0, 0.005)
+    pspace = Rectangle((min_x, min_y), (max_x, max_y))
+    r = mining_method(pspace, num_cells, num_samples, oracle)
+    r.plot_2D()
