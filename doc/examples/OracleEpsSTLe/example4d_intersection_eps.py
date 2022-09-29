5c182a03e97c245ba441da682d97343241a3a1a0
Akshay Mambakam <mambakaa@grepon.imag.fr>
2022-09-29 13:57:01 +0200

    [ECG 100] Fixed the script and template formulae to match ECG 100.

--- doc/examples/OracleEpsSTLe/example4d_intersection_eps.py
+++ doc/examples/OracleEpsSTLe/example4d_intersection_eps.py
@@ -53,7 +53,7 @@ def pareto_4d_intersection_eps(ecg_name, num_params, stl_template1, stl_template
     orac1 = OracleEpsSTLe(bound_on_count=bound1, intvl_epsilon=1)
     orac1.from_file(nfile1, human_readable)
 
-    orac2 = OracleEpsSTLe(bound_on_count=bound2, intvl_epsilon=1)
+    orac2 = OracleEpsSTLe(bound_on_count=bound2, intvl_epsilon=10)
     orac2.from_file(nfile2, human_readable)
 
     output_intersect = SearchIntersectionND_2(orac1, orac2,
@@ -72,27 +72,24 @@ def pareto_4d_intersection_eps(ecg_name, num_params, stl_template1, stl_template
 
 if __name__ == "__main__":
     # This script learns to detect pulses in ECGs. The detector consists of a Parametric STL formula with
-    # parameters (p1, p2, p3). The result of training the detector is the set of valuations of the parameter space such
+    # parameters (p1, p2, p3, p4). The result of training the detector is the set of valuations of the parameter space such
     # that the detector misclassifies 'bound1' false negatives and 'bound2' false positives.
     #
-    # In this example, the following call optimizes the detector for the ECG 221, with up to 0 false negatives, 1 false
-    # positives and a resolution of the parameter space of 1/10 (delta = 10). The opt_level = 2 means that the algorithm
-    # computes the satisfaction area of the parameter space (green boxes) instead of a single point.
+    # In this example, the following call optimizes the detector for the ECG 100, with up to 1 false negatives, 3 false
+    # positives and a resolution of the parameter space of 1/100 (delta = 100). The opt_level = 0 means that the algorithm
+    # computes a single point instead of the total satisfaction area of the parameter space (green boxes).
     #
-    # python3 ./example4d_intersection_eps.py 100 2 2 0 100
-    # ecg_name = str(sys.argv[1])
-    # bound1 = int(sys.argv[2])
-    # bound2 = int(sys.argv[3])
-    # opt_level = int(sys.argv[4])
-    # delta = 1.0 / float(sys.argv[5])
+    # python3 ./example4d_intersection_eps.py 100 1 6 0 100 # Time: 15s
+    # python3 ./example4d_intersection_eps.py 100 1 4 0 100 # Time 620s
+    # python3 ./example4d_intersection_eps.py 100 1 3 0 100 # Time: 1516s # Tightest
 
-    ecg_name = "100"
-    bound1 = 33
-    bound2 = 33
-    opt_level = 0
-    delta = 1.0 / 100.0
+    ecg_name = str(sys.argv[1])
+    bound1 = int(sys.argv[2])
+    bound2 = int(sys.argv[3])
+    opt_level = int(sys.argv[4])
+    delta = 1.0 / float(sys.argv[5])
 
-    min_tuple = (0.0, -1.0, -1.0, 0.0)
+    min_tuple = (0.0, -1.0, -1.0, 10.0)
     max_tuple = (70.0, 1.00, 1.0, 600.0)
     list_intervals = [(min_i, max_c) for min_i, max_c in zip(min_tuple, max_tuple)]
     list_constraints = []
