13a532fde1b4444c581bdc22b2960d09cea7d0b6
José Ignacio Requeno <nrequeno@unizar.es>
2022-10-01 16:29:30 +0200

    - Adapting methods for list of oracles instead of a single oracle - Fixing error in mining algorithm: it's enough that a single sample is True to make the cell green (i.e., replace 'all' by 'any')

--- doc/examples/mining_method.py
+++ doc/examples/mining_method.py
@@ -17,7 +17,7 @@ def confidence(p0: float, alpha: float) -> int:
     """As a concrete example, let us set p0 = 0.01 and alpha = 0.05. In this setting, we need to examine
     N >= log(1−p0) alpha = log0.99(0.05) = 298 parameter valuations to conclude with confidence 95% that the proportion
     of satisfied parameter valuation in the cell is smaller than 1%."""
-    n = log(alpha, 1-p0)
+    n = log(alpha, 1.0 - p0)
     return ceil(n)
 
 
@@ -25,7 +25,7 @@ def cell_partition(pspace: Rectangle, num_cells: int) -> List:
     d = pspace.dim()
 
     # num_cells = k^d
-    k = log(num_cells, 10)/d
+    k = log(num_cells, 10) / d
     k = ceil(pow(10, k))
 
     step = np.subtract(pspace.max_corner, pspace.min_corner)
@@ -37,48 +37,56 @@ def cell_partition(pspace: Rectangle, num_cells: int) -> List:
     return res
 
 
-# Inicializacion del Oracle
-# Num_muestras
-def mining_method(pspace: Rectangle, num_cells: int, num_samples: int, oracle: OracleSTLeLib) -> ResultSet:
+def test_sample(sample: iter, fs: list[callable]) -> bool:
+    return all(f(sample) for f in fs)
+
+
+def mining_method(pspace: Rectangle, num_cells: int, num_samples: int, oracles: list[OracleSTLeLib]) -> ResultSet:
     cells = cell_partition(pspace, num_cells)
     undef = []
     green = []
     red = []
 
     d = pspace.dim()
-    f = oracle.membership()
+    fs = [ora.membership() for ora in oracles]
     for cell in cells:
         # Take num_samples uniformly between cell.min_corner and cell.max_corner
         samples = np.random.uniform(cell.min_corner, cell.max_corner, (num_samples, d))
-        # Call the oracle with the current sample
-        eval_samples = (f(sample) for sample in samples)
-        # If all the samples eval to True, then the cell goes to green. Otherwise, cell goes to red.
-        if all(eval_samples):
+        # Call the oracles with the current sample
+        if any(test_sample(sample, fs) for sample in samples):
+            # If all the signals eval a sample to True, then the cell goes to green.
+            # It suffices that a single sample is validated by all the signals.
             green.append(cell)
         else:
+            # Otherwise, the cell goes to red.
             red.append(cell)
 
     return ResultSet(undef, red, green, pspace)
 
 
-# def process(cell: Rectangle, f: callable, num_samples: int, d: int) -> bool:
-def process(args: tuple) -> bool:
-    cell, oracle, num_samples, d = args
+def par_test_sample(args: tuple[iter, list[callable]]) -> bool:
+    sample, fs = args
+    return all(f(sample) for f in fs)
 
-    f = oracle.membership()
+
+def process(args: tuple[Rectangle, list[OracleSTLeLib], int, int]) -> bool:
+    cell, oracles, num_samples, d = args
+
+    fs = [ora.membership() for ora in oracles]
 
     # Take num_samples uniformly between cell.min_corner and cell.max_corner
     samples = np.random.uniform(cell.min_corner, cell.max_corner, (num_samples, d))
     # Call the oracle with the current sample
-    eval_samples = (f(sample) for sample in samples)
+    res = any(test_sample(sample, fs) for sample in samples)
+
+    # args = ((copy.deepcopy(sample), copy.deepcopy(fs)) for sample in samples)
     # executor = ThreadPoolExecutor(max_workers=5)
-    # eval_samples = executor.map(f, samples)
-    # If all the samples eval to True, then the cell goes to green. Otherwise, cell goes to red.
-    return all(eval_samples)
+    # res = any(executor.map(par_test_sample, args))
+
+    return res
 
 
-def par_mining_method(pspace: Rectangle, num_cells: int, num_samples: int, oracle: OracleSTLeLib) -> ResultSet:
-
+def par_mining_method(pspace: Rectangle, num_cells: int, num_samples: int, oracles: list[OracleSTLeLib]) -> ResultSet:
     cells = cell_partition(pspace, num_cells)
     undef = []
     green = []
@@ -87,7 +95,7 @@ def par_mining_method(pspace: Rectangle, num_cells: int, num_samples: int, oracl
     d = pspace.dim()
 
     p = Pool(cpu_count())
-    args = ((cell, copy.deepcopy(oracle), num_samples, d) for cell in cells)
+    args = ((cell, copy.deepcopy(oracles), num_samples, d) for cell in cells)
     green_cells = p.map(process, args)
     for i, cell in enumerate(cells):
         if green_cells[i]:
@@ -111,6 +119,7 @@ if __name__ == "__main__":
     nfile = '../../Tests/Oracle/OracleSTLe/2D/stabilization/derivative/stabilization.txt'
     oracle = OracleSTLeLib()
     oracle.from_file(nfile, human_readable=True)
+    oracles = [copy.deepcopy(oracle)] * 1
 
     num_samples = confidence(p0, alpha)
     # pspace = Rectangle((0.0,) * 3, (1.0,) * 3)
@@ -119,14 +128,13 @@ if __name__ == "__main__":
     pspace = Rectangle((min_x, min_y), (max_x, max_y))
 
     start_time = time.time()
-    r = par_mining_method(pspace, num_cells, num_samples, oracle)
+    r = par_mining_method(pspace, num_cells, num_samples, oracles)
     end_time = time.time()
     print("Execution time: {0}".format(end_time - start_time))
-
     r.plot_2D()
 
     start_time = time.time()
-    r = mining_method(pspace, num_cells, num_samples, oracle)
+    r = mining_method(pspace, num_cells, num_samples, oracles)
     end_time = time.time()
     print("Execution time: {0}".format(end_time - start_time))
     r.plot_2D()
