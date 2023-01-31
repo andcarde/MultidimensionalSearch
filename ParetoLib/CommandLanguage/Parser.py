27a65431e6bf9666f12b77825e1480dd6b138dc6
nachorequeno <jrequeno@ucm.es>
2023-01-31 20:30:04 +0100

    Cleaning Parser.py

--- ParetoLib/CommandLanguage/Parser.py
+++ ParetoLib/CommandLanguage/Parser.py
@@ -3,109 +3,132 @@ import ParetoLib.CommandLanguage.Lexer as lexer
 
 tokens = lexer.tokens
 
+
 def p_let_word(t):
     '''
     r'let'
     '''
     t[0] = ('LET_WORD', t[0])
+
+
 def p_param_word(t):
     '''
     r'param'
     '''
     t[0] = ('PARAM_WORD', t[0])
 
+
 def p_signal_word(t):
     '''
     r'signal'
     '''
     t[0] = ('SIGNAL_WORD', t[0])
+
+
 def p_probabilistic_word(t):
     '''
     r'probabilistic'
     '''
     t[0] = ('PROBABILISTIC_WORD', t[0])
+
+
 def p_semicolon(t):
     '''
     r';'
     '''
     t[0] = ('SEMICOLON', t[0])
+
+
 def p_param_list(t):
     '''
     PARAM_LIST = ID_LIST
     '''
     t[0] = ('PARAM_LIST', t[0])
+
+
 def p_signal_list(t):
     '''
     SIGNAL_LIST = ID_LIST
     '''
     t[0] = ('SIGNAL_LIST', t[0])
+
+
 def p_probsignal_list(t):
     '''
     PROBSIGNAL_LIST = ID_LIST
     '''
     t[0] = ('PROBSIGNAL_LIST', t[0])
+
+
 def p_def_param(t):
     '''
     DEF_PARAM = LET_WORD + PARAM_WORD + PARAM_LIST + SEMICOLON
     '''
     t[0] = ('PARAM', t[0])
+
+
 def p_def_signal(t):
     '''
     DEF_SIGNAL = LET_WORD + SIGNAL_WORD + SIGNAL_LIST + SEMICOLON
     '''
     t[0] = ('SIGNAL', t[0])
+
+
 def p_def_probsignal(t):
     '''
     DEF_PROBSIGNAL = LET_WORD + PROBABILISTIC_WORD + SIGNAL_WORD + PROBSIGNAL_LIST + SEMICOLON
     '''
     t[0] = ('PROBSIGNAL', t[0])
+
+
 def p_id_list(t):
     '''
     ID_LIST = ID | ID, ID_LIST
     '''
     t[0] = ('ID_LIST', t[0])
+
+
 def p_id(t):
     '''
     r'[\w+][\d*]'
     '''
     t[0] = ('ID', t[0])
+
+
 def p_eval(t):
     '''
-    eval ID on SIGNAL_LIST with INTVL_LIST | eval ID with INTVL_LIST on SIGNAL_LIST |
-        ID on PROBSIGNAL_LIST with INTVL_LIST | eval ID with INTVL_LIST on PROBSIGNAL_LIST |
+    EVAL = ID ON SIGNAL_LIST WITH INTVL_LIST |
+           ID ON PROBSIGNAL_LIST WITH INTVL_LIST |
+           ID WITH INTVL_LIST ON SIGNAL_LIST |
+           ID WITH INTVL_LIST ON PROBSIGNAL_LIST
     '''
     t[0] = ('EVAL', t[0])
+
+
 def p_intvl_list(t):
     '''
-    INTVL_LIST = ID in INTVL | ID in INTVL, INTVL_LIST
+    INTVL_LIST = ID IN INTVL |
+                 ID IN INTVL COMMA INTVL_LIST
     '''
     t[0] = ('INTVL_LIST', t[0])
+
+
 def p_intvl(t):
     '''
     INTVL = LBRACKET [NUMBER | ID ] COMMA [NUMBER | ID] RBRACKET
     '''
     t[0] = ('INTVL', t[0])
-def p_comma(t):
-    '''
-    r','
-    '''
-    t[0] = ('COMMA', t[0])
-def p_lbracket(t):
-    '''
-    r'['
-    '''
-    t[0] = ('LBRACKET', t[0])
-def p_rbracket(t):
-    '''
-    r']'
-    '''
-    t[0] = ('RBRACKET', t[0])
+
+
+
 def p_number(t):
     '''
     r'[-]?[\d+].[\d+]'
     '''
     t.value = float(t.value)
     t[0] = ('NUMBER', t[0])
+
+
 # Write the matching regex in the docstring.
 # Cuando hacemos t.value almacenamos información en dicho patrón
 # El (int), castea el texto en un integer (tipo de Python)
@@ -115,184 +138,213 @@ def t_integer(t):
     '''
     t.value = int(t.value)
     return t
+
+
 def p_spec_file(t):
     '''
     SPEC_FILE = [DEF_SIGNAL | DEF_PROBSIGNAL]?
 	    [DEF_PARAM]? [PROP_LIST]+ [EVAL]+
     '''
     t[0] = ('SPEC_FILE', t[0])
+
+
 def prop_list(t):
     '''
     PROP_LIST = PROP | PROP + PROP_LIST
     '''
     t[0] = ('PROP_LIST', t[0])
+
+
 def p_prop(t):
     '''
     PROP = ID := [PHI | PSI] [SEMICOLON]?
     '''
     t[0] = ('PROP', t[0])
+
+
 def p_phi(t):
     '''
     PHI : ID | FUNC | NOT PHI | PROB PHI | PHI BIN_BOOL_OP PHI | F[INTVL]? PHI
         | G[INTVL]? PHI | PHI U[INTVL]? PHI | ON[INTVL] PSI | LPAR PHI RPAR
     '''
     t[0] = ('PHI', t[0])
+
+
 def p_on(t):
     '''
     r'on'
     '''
     t[0] = ('ON', t[0])
-def p_lpar(t):
-    '''
-    r'('
-    '''
-    t[0] = ('LPAR', t[0])
-def p_rpar(t):
-    '''
-    r')'
-    '''
-    t[0] = ('RPAR', t[0])
+
+
 def p_psi(t):
     '''
     PSI = min PHI | max PHI | integral PHI | der PHI
     '''
     t[0] = ('PSI', t[0])
+
+
 def p_func(t):
     '''
     FUNC = SIG BIN_COND SIG | SIG BIN_OP SIG
     '''
     t[0] = ('FUNC', t[0])
-# Greater op, greater or equal op, less op, less or equal..
+
+
 def p_bin_bool_op(t):
     '''
-    BIN_BOOL_OP = GR_OP | GRE_OP, LE_OP, LEE_OP
+    BIN_BOOL_OP = PHI AND PHI |
+                  PHI OR PHI |
+                  PHI IMPLY PHI
     '''
-    t[0] = ('BIN_BOOL_OP', t[0])
-# Tengo que añadir la suma, resta, mul., div. (operaciones binarias)
+    #       OP    PHI1  PHI2
+    t[0] = (t[2], t[1], t[3])
+
+
 def p_bin_cond(t):
     '''
-    BIN_COND = ADD_OP | SUS_OP | MUL_OP | DIV_OP
+    BIN_COND = SIG LEQ SIG |
+               SIG LESS SIG |
+               SIG GEQ SIG |
+               SIG GREATER SIG
     '''
-    t[0] = ('BIN_COND', t[0])
-# Tengo que añadir AND, OR, IMPLICATE
-def p_bin_bool_op(t):
+    #       OP    SIG1  SIG2
+    t[0] = (t[2], t[1], t[3])
+
+
+def p_bin_arith_op(t):
     '''
-    BIN_COND = ...
+    BIN_COND = SIG PLUS SIG |
+               SIG MINUS SIG |
+               SIG TIMES SIG |
+               SIG DIVIDE SIG
     '''
-    t[0] = ('BIN_COND', t[0])
+    #       OP    SIG1  SIG2
+    t[0] = (t[2], t[1], t[3])
+
+
 def p_sig(t):
     '''
     SIG = ID | CONSTANT_SIGNAL
     '''
     t[0] = ('SIG', t[0])
+
+
 def p_constant_signal(t):
     '''
     CONSTANT_SIGNAL = NUMBER
     '''
     t[0] = ('CONSTANT_SIGNAL', t[0])
-def p_let_word(t):
-    '''
-    r'let'
-    '''
-    t[0] = ('LET_WORD', t[0])
+
+# Funciones repetidas desde aquí hacia abajo
+
 def p_param_word(t):
     '''
     r'param'
     '''
     t[0] = ('PARAM_WORD', t[0])
 
+
 def p_signal_word(t):
     '''
     r'signal'
     '''
     t[0] = ('SIGNAL_WORD', t[0])
+
+
 def p_probabilistic_word(t):
     '''
     r'probabilistic'
     '''
     t[0] = ('PROBABILISTIC_WORD', t[0])
-def p_semicolon(t):
-    '''
-    r';'
-    '''
-    t[0] = ('SEMICOLON', t[0])
+
+
 def p_param_list(t):
     '''
     PARAM_LIST = ID_LIST
     '''
     t[0] = ('PARAM_LIST', t[0])
+
+
 def p_signal_list(t):
     '''
     SIGNAL_LIST = ID_LIST
     '''
     t[0] = ('SIGNAL_LIST', t[0])
+
+
 def p_probsignal_list(t):
     '''
     PROBSIGNAL_LIST = ID_LIST
     '''
     t[0] = ('PROBSIGNAL_LIST', t[0])
+
+
 def p_def_param(t):
     '''
     DEF_PARAM = LET_WORD + PARAM_WORD + PARAM_LIST + SEMICOLON
     '''
     t[0] = ('PARAM', t[0])
+
+
 def p_def_signal(t):
     '''
     DEF_SIGNAL = LET_WORD + SIGNAL_WORD + SIGNAL_LIST + SEMICOLON
     '''
     t[0] = ('SIGNAL', t[0])
+
+
 def p_def_probsignal(t):
     '''
     DEF_PROBSIGNAL = LET_WORD + PROBABILISTIC_WORD + SIGNAL_WORD + PROBSIGNAL_LIST + SEMICOLON
     '''
     t[0] = ('PROBSIGNAL', t[0])
+
+
 def p_id_list(t):
     '''
     ID_LIST = ID | ID, ID_LIST
     '''
     t[0] = ('ID_LIST', t[0])
+
+
 def p_id(t):
     '''
     r'[\w+][\d*]'
     '''
     t[0] = ('ID', t[0])
+
+
 def p_eval(t):
     '''
     eval ID on SIGNAL_LIST with INTVL_LIST | eval ID with INTVL_LIST on SIGNAL_LIST |
         ID on PROBSIGNAL_LIST with INTVL_LIST | eval ID with INTVL_LIST on PROBSIGNAL_LIST |
     '''
     t[0] = ('EVAL', t[0])
+
+
 def p_intvl_list(t):
     '''
     INTVL_LIST = ID in INTVL | ID in INTVL, INTVL_LIST
     '''
     t[0] = ('INTVL_LIST', t[0])
+
+
 def p_intvl(t):
     '''
     INTVL = LBRACKET [NUMBER | ID ] COMMA [NUMBER | ID] RBRACKET
     '''
     t[0] = ('INTVL', t[0])
-def p_comma(t):
-    '''
-    r','
-    '''
-    t[0] = ('COMMA', t[0])
-def p_lbracket(t):
-    '''
-    r'['
-    '''
-    t[0] = ('LBRACKET', t[0])
-def p_rbracket(t):
-    '''
-    r']'
-    '''
-    t[0] = ('RBRACKET', t[0])
+
+
 def p_number(t):
     '''
     r'[-]?[\d+].[\d+]'
     '''
     t.value = float(t.value)
     t[0] = ('NUMBER', t[0])
+
+
 # Write the matching regex in the docstring.
 # Cuando hacemos t.value almacenamos información en dicho patrón
 # El (int), castea el texto en un integer (tipo de Python)
@@ -302,81 +354,89 @@ def t_integer(t):
     '''
     t.value = int(t.value)
     return t
+
+
 def p_spec_file(t):
     '''
     SPEC_FILE = [DEF_SIGNAL | DEF_PROBSIGNAL]?
 	    [DEF_PARAM]? [PROP_LIST]+ [EVAL]+
     '''
     t[0] = ('SPEC_FILE', t[0])
+
+
 def prop_list(t):
     '''
     PROP_LIST = PROP | PROP + PROP_LIST
     '''
     t[0] = ('PROP_LIST', t[0])
+
+
 def p_prop(t):
     '''
     PROP = ID := [PHI | PSI] [SEMICOLON]?
     '''
     t[0] = ('PROP', t[0])
+
+
 def p_phi(t):
     '''
     PHI : ID | FUNC | NOT PHI | PROB PHI | PHI BIN_BOOL_OP PHI | F[INTVL]? PHI
         | G[INTVL]? PHI | PHI U[INTVL]? PHI | ON[INTVL] PSI | LPAR PHI RPAR
     '''
     t[0] = ('PHI', t[0])
-def p_on(t):
-    '''
-    r'on'
-    '''
-    t[0] = ('ON', t[0])
-def p_lpar(t):
-    '''
-    r'('
-    '''
-    t[0] = ('LPAR', t[0])
-def p_rpar(t):
-    '''
-    r')'
-    '''
-    t[0] = ('RPAR', t[0])
+
 def p_psi(t):
     '''
     PSI = min PHI | max PHI | integral PHI | der PHI
     '''
     t[0] = ('PSI', t[0])
+
+
 def p_func(t):
     '''
     FUNC = SIG BIN_COND SIG | SIG BIN_OP SIG
     '''
     t[0] = ('FUNC', t[0])
+
+
 # Greater op, greater or equal op, less op, less or equal..
 def p_bin_bool_op(t):
     '''
     BIN_BOOL_OP = GR_OP | GRE_OP, LE_OP, LEE_OP
     '''
     t[0] = ('BIN_BOOL_OP', t[0])
+
+
 # Tengo que añadir la suma, resta, mul., div. (operaciones binarias)
 def p_bin_cond(t):
     '''
     BIN_COND = ADD_OP | SUS_OP | MUL_OP | DIV_OP
     '''
     t[0] = ('BIN_COND', t[0])
+
+
 # Tengo que añadir AND, OR, IMPLICATE
 def p_bin_bool_op(t):
     '''
     BIN_COND = ...
     '''
     t[0] = ('BIN_COND', t[0])
+
+
 def p_sig(t):
     '''
     SIG = ID | CONSTANT_SIGNAL
     '''
     t[0] = ('SIG', t[0])
+
+
 def p_constant_signal(t):
     '''
     CONSTANT_SIGNAL = NUMBER
     '''
     t[0] = ('CONSTANT_SIGNAL', t[0])
+
+# Funciones repetidas desde aquí hacia arriba
 
 def p_not(t):
     '''
@@ -450,6 +510,7 @@ def t_ignore_newline(t):
 def t_error(t):
     print(f'Illegal character {t.value[0]!r}')
     t.lexer.skip(1)
+
 
 # let prop := t1, t2, t3, t11, t21, t34
 
