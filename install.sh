28249169865e9a760000c1f794122dc9bd18101c
ignaciofabregas <fabregas@ucm.es>
2023-03-29 12:03:13 +0200

    typo en install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # pip3 uninstall ParetoLib
 pip3 install -r requirements.txt
 python3 setup.py bdist_wheel --universal
-pip3 install dis/*.whl
+pip3 install dist/*.whl
 # ----------------------------
 # pip3 install .
 # ----------------------------
