9bb0b83ee69d117f1f230885abd95a0fc8c64554
José Ignacio Requeno <nrequeno@unizar.es>
2023-04-12 13:58:18 +0200

    Cleaning temporary files

--- .ipynb_checkpoints/install_cython-checkpoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-rm ParetoLib/*/*.c
-#pip3 uninstall ParetoLib
-pip3 install -r requirements.txt
-python3 setup_cython.py clean --all
-python3 setup_cython.py build_ext --inplace
-python3 setup_cython.py bdist_wheel --universal
-python3 setup_cython.py bdist_egg
-pip3 install dist/ParetoLib-2.2.0-*10*linux*whl
