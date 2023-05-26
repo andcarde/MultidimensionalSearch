a8314ccf89a3007d622b284b1851d0267da7e047
José Ignacio Requeno <nrequeno@unizar.es>
2023-05-26 11:17:35 +0200

    Rewritting and simplification of OracleDpoly.py. Repeat the same for OracleIpoly.py + factorize the modules (i.e., move OracleXpoly.py to another submodule?)

--- ParetoLib/Oracle/OracleDpoly.py
+++ ParetoLib/Oracle/OracleDpoly.py
@@ -13,6 +13,7 @@ Checks if: (1*p[0]+2*p[1]+3*p[2]<=8) and (5*p[0]+4*p[1]+7*p[2]<=10)
 """
 from ParetoLib.Oracle.Oracle import Oracle
 
+
 class OracleDpoly(Oracle):
     def __init__(self, constraints):
         # type: (OracleDpoly, list) -> None
@@ -20,23 +21,19 @@ class OracleDpoly(Oracle):
         # super(OracleDpoly, self).__init__()
         Oracle.__init__(self)
 
-        # constraints is a list of tuples with the same dimension
-        # First check if the constraints are all of same dimension
-        d = 0
-        for cons in constraints:
-            if(d == 0):
-                d = len(cons)
-            else:
-                if(len(cons)!=d):
-                    # Throw error here
-                    print("All constraints need to have the same dimension!!!")
-                    exit(0)
+        # Constraints is a list of tuples with the same dimension:
+        # constraints = [cons_1, cons_2, ..., cons_n]
+        # p1 + p2 + 3*p3 <= 8
+        # cons_1 = (1, 1, 3, 8)
+
+        cons = constraints[0]
+        d = len(cons)
+
+        # Check if the constraints are all of same dimension
+        assert all(len(cons) == d for cons in constraints), "All constraints need to have the same dimension!!!"
+
         # Check if all the coefficients are non-negative
-        for cons in constraints:
-            for i in range(0,len(cons)-1):
-                if(cons[i]<0):
-                    print("Coefficients should be non-negatives!!!")
-                    exit(0)
+        assert all(ci > 0 for cons in constraints for ci in cons), "Coefficients should be non-negatives!!!"
 
         self.constraints = constraints
 
@@ -45,12 +42,17 @@ class OracleDpoly(Oracle):
         """
         See Oracle.member().
         """
+
+        # d = len(point)
+        # d + 1 == len(cons)
+
         res = True
         for cons in self.constraints:
-            sum = 0
-            for i in range(0,len(cons)-1):
-                sum += cons[i]*point[i]
-            res = res and (sum <= cons[-1])
+            pair = zip(cons, point)
+            addition = sum(cons_i * point_i for (cons_i, point_i) in pair)
+
+            # Last item of the cons tuple represents the boundary
+            res = res and (addition <= cons[-1])
         return res
 
     def dim(self):
@@ -58,7 +60,7 @@ class OracleDpoly(Oracle):
         """
         See Oracle.dim().
         """
-        if (len(self.constraints)!= 0):
+        if len(self.constraints) != 0:
             return len(self.constraints[0])-1
         else:
             return -1
