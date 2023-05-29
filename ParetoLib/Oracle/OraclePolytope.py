112d5fc1145048240e7225e79db8f12bb06473df
José Ignacio Requeno <nrequeno@unizar.es>
2023-05-29 18:53:21 +0200

    Refactoring OracleDpoly.py and OracleIpoly.py into OraclePolytope.py

--- ParetoLib/Oracle/OraclePolytope.py
+++ ParetoLib/Oracle/OraclePolytope.py
@@ -14,9 +14,9 @@ Checks if: (1*p[0]+2*p[1]+3*p[2]<=8) and (5*p[0]+4*p[1]+7*p[2]<=10)
 from ParetoLib.Oracle.Oracle import Oracle
 
 
-class OracleDpoly(Oracle):
+class OraclePolytope(Oracle):
     def __init__(self, constraints):
-        # type: (OracleDpoly, list) -> None
+        # type: (OraclePolytope, list) -> None
 
         # super(OracleDpoly, self).__init__()
         Oracle.__init__(self)
@@ -37,8 +37,40 @@ class OracleDpoly(Oracle):
 
         self.constraints = constraints
 
+    def dim(self):
+        # type: (OraclePolytope) -> int
+        """
+        See Oracle.dim().
+        """
+        if len(self.constraints) != 0:
+            return len(self.constraints[0])-1
+        else:
+            return -1
+
+
+class OracleIncreasing(OraclePolytope):
     def member(self, point):
-        # type: (OracleDpoly, tuple) -> bool
+        # type: (OraclePolytope, tuple) -> bool
+        """
+        See Oracle.member().
+        """
+
+        # d = len(point)
+        # d + 1 == len(cons)
+
+        res = True
+        for cons in self.constraints:
+            pair = zip(cons, point)
+            addition = sum(cons_i * point_i for (cons_i, point_i) in pair)
+
+            # Last item of the cons tuple represents the boundary
+            res = res and (addition >= cons[-1])
+        return res
+
+
+class OracleDecreasing(OraclePolytope):
+    def member(self, point):
+        # type: (OraclePolytope, tuple) -> bool
         """
         See Oracle.member().
         """
@@ -54,13 +86,3 @@ class OracleDpoly(Oracle):
             # Last item of the cons tuple represents the boundary
             res = res and (addition <= cons[-1])
         return res
-
-    def dim(self):
-        # type: (OracleDpoly) -> int
-        """
-        See Oracle.dim().
-        """
-        if len(self.constraints) != 0:
-            return len(self.constraints[0])-1
-        else:
-            return -1
