66eb04d6f6d971cea3862a62d2be7b2b104d5b73
José Ignacio Requeno <nrequeno@unizar.es>
2023-05-31 10:46:50 +0200

    Refactoring folders

--- doc/examples/OraclePolytope/example_fast_robust_intersection.py
+++ doc/examples/OraclePolytope/example_fast_robust_intersection.py
@@ -14,25 +14,25 @@ nfile2 = './fordec.txt'
 human_readable = True
 
 # Definition of the n-dimensional space
-list_intervals = [(0,100),(0,100)]
+list_intervals = [(0, 100), (0, 100)]
 
-ora1 = OracleIncreasingPolytope([(1,1,100)])
-ora2 = OracleDecreasingPolytope([(1,1,130)])
+ora1 = OracleIncreasingPolytope([(1, 1, 100)])
+ora2 = OracleDecreasingPolytope([(1, 1, 130)])
 ora3 = OracleSTLeLib()
 ora3.from_file(nfile1, human_readable)
 ora4 = OracleSTLeLib()
 ora4.from_file(nfile2, human_readable)
 
 rs = SearchRobustIntersectionND_2(ora1, ora2,
-                           ora3, ora4,
-                           list_intervals,
-                           epsilon=EPS,
-                           delta=0.01,
-                           max_step=STEPS,
-                           blocking=False,
-                           sleep=0.0,
-                           opt_level=2,
-                           parallel=False,
-                           logging=True,
-                           simplify=False)
+                                  ora3, ora4,
+                                  list_intervals,
+                                  epsilon=EPS,
+                                  delta=0.01,
+                                  max_step=STEPS,
+                                  blocking=False,
+                                  sleep=0.0,
+                                  opt_level=2,
+                                  parallel=False,
+                                  logging=True,
+                                  simplify=False)
 rs.plot_2D()
